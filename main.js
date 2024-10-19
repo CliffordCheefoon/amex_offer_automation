8e8b7a42c451f7243705f28931eb5f28729dac1f
Clifford Cheefoon <cheefoonclifford@yahoo.com>
2024-10-18 20:57:37 -0400

    possibly a better version?

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-// Find all the "Add to Card" buttons on the page. NOTE: The button class changes based on aspect ratio
-var buttonClass = "offer-cta axp-offers__global__merchantOfferCta___qKBIE btn btn-size-md btn-secondary offer-cta-v css-261oxr"
-var offerButtons = Array.from(document.getElementsByClassName(buttonClass)).filter(btn => btn.title == "Add to Card");
-console.log(offerButtons)
+var offerButtons = Array.from(document.querySelectorAll('button[title="Add to Card"]'));
 var index;
 for (index = 0; index < offerButtons.length; ++index) {
     console.log("Clicking offer button");
     offerButtons[index].click();
-    // Wait to be nice to AMEX servers :)
-    await new Promise(r => setTimeout(r, 500));
+    // Wait  to be considerate to the AMEX servers
+    await new Promise(r => setTimeout(r, 1000));
 }
